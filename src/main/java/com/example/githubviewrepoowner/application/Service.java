bd605e67eab622582036747ae95fc86c3c40a970
FilipBianga <fbianga94@gmail.com>
2023-06-09 20:50:28 +0200

    Added Exception handler to error 404

--- src/main/java/com/example/githubviewrepoowner/application/Service.java
+++ src/main/java/com/example/githubviewrepoowner/application/Service.java
@@ -1,20 +1,8 @@
 package com.example.githubviewrepoowner.application;
 
 import com.example.githubviewrepoowner.domain.Repository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
 
-@org.springframework.stereotype.Service
-@AllArgsConstructor
-public class Service {
-    private static final String APIGITHUB = "https://api.github.com/users/";
+public interface Service {
 
-    @Autowired
-    RestTemplate restTemplate;
-
-
-    public Repository[] allRepo(String username) {
-        return restTemplate.getForObject(APIGITHUB + username + "/repos", Repository[].class);
-    }
+    Repository[] allRepo(String username);
 }
--- src/main/java/com/example/githubviewrepoowner/domain/Repository.java
+++ src/main/java/com/example/githubviewrepoowner/domain/Repository.java
@@ -3,16 +3,12 @@ package com.example.githubviewrepoowner.domain;
 import lombok.*;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Repository {
 
-    private long id;
     private OwnerLogin owner;
     private String name;
-
     private Boolean fork;
-
     private String branches_url;
 }
